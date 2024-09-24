136a3cd1547f4ade491ea19b9ef25daec3ea42a1
Sarunwit00 <a.sarunwit@gmail.com>
2024-09-24 11:51:24 +0700

    Save_4

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ session_start();
 <body>
     <div class="container">
     <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
-    <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
+    <?php include "nav.php" ?>
+    <!-- <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i>Home</a>
       <ul class="navbar-nav">
@@ -36,7 +37,7 @@ session_start();
         ?>
       </ul>
     </div>
-</nav>
+</nav> -->
 <div class="mt-3">
     <label>หมวดหมู่:</label>
     <span class="dropdown">
