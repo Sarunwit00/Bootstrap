9d047d647f51026abc70ecd7447552f5569f11fb
Sarunwit00 <a.sarunwit@gmail.com>
2024-09-24 09:32:59 +0700

    save_1

--- index.php
+++ index.php
@@ -30,16 +30,13 @@ session_start();
         <!-- <li class="nav-item">
           <a class="nav-link" href="login.php"><i class="bi bi-pencil"></i>เข้าสู่ระบบ</a>
         </li> -->
-        <!-- <li class="nav-item dropdown">
+        <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+          <i class="bi bi-person-lines-fill"></i> admin
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul> -->
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
       </ul>
    
   </div>
