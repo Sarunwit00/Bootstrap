772b3ad3faef3649066b52bf4af0b4d6db76dd3c
Sarunwit00 <a.sarunwit@gmail.com>
2024-10-01 10:03:50 +0700

    save

--- index.php
+++ index.php
@@ -15,29 +15,7 @@ session_start();
     <div class="container">
     <h1 style="text-align: center;" class="mt-3">Webboard KakKak</h1>
     <?php include "nav.php" ?>
-    <!-- <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i>Home</a>
-      <ul class="navbar-nav">
-        <?php
-            if (!isset($_SESSION['id'])){
-                echo "<li class='nav-item'>";
-                    echo "<a class='nav-link' href='login.php'><i class='bi bi-pencil'></i>เข้าสู่ระบบ</a>";
-                echo "</li>";
-            }else{
-                echo "<li class='nav-item dropdown'>";
-                  echo "<a class='btn btn-outline-secondary btm-sm dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>";
-                  echo "<i class='bi bi-person-lines-fill'></i> $_SESSION[username] </a>";
-                  echo "<ul class='dropdown-menu'>";
-                    echo "<li><a class='dropdown-item' href='logout.php'><i class='bi bi-power'></i> ออกจากระบบ</a></li>";
-                  echo "</ul>";
-                echo "</li>";
-                
-            }
-        ?>
-      </ul>
-    </div>
-</nav> -->
+
 <div class="mt-3">
     <label>หมวดหมู่:</label>
     <span class="dropdown">
